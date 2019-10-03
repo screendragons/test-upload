d56e9dbd6c64b174644aa517048681b8bb496f21
screendragons <chiyuyeung@outlook.com>
2019-10-03 15:05:32 +0200

    update

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -62,68 +62,20 @@ class TestController extends Controller
         // $uploads = auth()->user()->uploads()->create($request->all());
 
 
+        // $upload = new Upload();
+        // $upload->name = $request->input('name');
+        // $upload->file = $request->input('file');
 
-        $upload = new Upload();
-        $upload->name = $request->input('name');
-        $upload->file = $request->input('file');
+        // if($request->hasFile('image')) {
+        //     $file = $request->file('image');
+        //     $extension = $file->getClientOriginalName();
+        //     $file->move('/app/public/'.$filename);
+        //     $public->image = $filename;
+        // }
 
-        if($request->hasFile('image')) {
-            $file = $request->file('image');
-            $extension = $file->getClientOriginalName();
-            $file->move('/app/public/'.$filename);
-            $public->image = $filename;
-        }
-
-
-
-        // oude code van project
-        $this->validate($request, [
-            'title' => 'required',
-            'body' => 'required',
-            'file' => 'image|nullable|max:1999'
-        ]);
-
-        // Handle file upload
-        if($request->hasFile('file')){
-            // dd('here');
-            // Get file name with extension
-            $filenameWithExt = $request->file('file')->getClientOriginalName();
-            // Get just filename
-            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-            // Get just ext
-            $extension = $request->file('file')->getClientOriginalName();
-            // Filename to store
-            $fileNameToStore = str_slug($filename.'_'.time()).'.'.$extension;
-            // Upload Image
-            Image::make($request->file('file'))
-                ->resize(600, 300, function($constraint){
-                    $constraint->aspectRatio();
-                })
-                ->save(storage_path('app\\public\\'.$fileNameToStore));
-                // ->storeAs('public', $fileNameToStore);
-            // $path = $request->file('file')->storeAs('public', $fileNameToStore);
-            // $image = $request->file('file');
-            // $imageName = $image->getClientOriginalName();
-            // $image->move(public_path('images'),$filenameWithExt);
-        } else {
-            $fileNameToStore = 'no_image.jpg';
-        }
-
-        $upload = new Upload();
-        $upload->user_id = Auth::id();
-        $upload->name = $request->title;
-        $upload->filename = $fileNameToStore;
-        $upload->description = $request->body;
-        $upload->media_type = '';
-        $upload->datasize = 1;
-        $upload->save();
-
-        // dd($upload);
-
-        // return redirect()->back();
-         return redirect('home');
-            // ->with('uploads', $upload);
-
+        $file = $request->file('image')->storeAs(
+            'images', $request->user()->id
+        );
     }
 
     /**
